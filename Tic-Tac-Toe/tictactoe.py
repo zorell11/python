c9f9460a304ce4722064980f69e163725dddb247
Zoltan Veres <zveres@dxc.com>
2020-08-18 14:52:12 +0200

    diag check and print board changed

--- Tic-Tac-Toe/tictactoe.py
+++ Tic-Tac-Toe/tictactoe.py
@@ -1,10 +1,8 @@
-import random
-
 BOARD = [['', '', ''],
         ['', '', ''],
         ['', '', '']]
-PLAYER_X = 'x'
-PLAYER_O = 'o'
+PLAYER_X = 'X'
+PLAYER_O = 'O'
 
 def print_rules():
     print('''
@@ -20,12 +18,18 @@ The WINNER is who succeeds in placing three of their marks in a
 Let's start the game
     ''')
 
+# print the BOARD
 def print_board(board):
-    print( 6 * '-')
+    whole_board = []
     for i in board:
-        print('{:1}|{:1}|{:1}'.format(*i))
-        print( 6 * '-')
+        line = '{:1}|{:1}|{:1}'.format(*i)
+        whole_board.append(line)
+    for i in range(1, len(board)+1, 2):
+        whole_board.insert(i, '-'*len(line))
+    whole_board  = '\n'.join(whole_board)
+    print(whole_board)
 
+# read and check players move
 def player_move(board, player):
     print(39 * '=')
     while True:
@@ -43,32 +47,39 @@ def player_move(board, player):
         except IndexError:
             print('Enter number from 1 to 9')
 
+# check rows
 def check_row(BOARD, row, player):
     for i in BOARD[row]:
         if i != player:
             return False
     return True
 
+# check columns
 def check_coulumn(BOARD, column, player):
     for i in range(len(BOARD)):
         if BOARD[i][column] != player:
             return False
     return True
 
-def check_diag1(BOARD,player):
-    for i in range(0, 9, 4):
-        x, y = divmod(i, len(BOARD))
-        if BOARD[x][y] != player:
-            return False
-    return True
+# transform nested list to one list
+def flatten(board):
+    lst = []
+    for i in board:
+        lst += i
+    return lst
 
-def check_diag2(BOARD,player):
-    for j in range(2, 7, 2):
-        x, y = divmod(j, len(BOARD))
-        if BOARD[x][y] != player:
-            return False
-    return True
+# check diagonals
+def check_diag(BOARD, player):
+    flat_board = flatten(BOARD)
+    size = len(BOARD)
+    #check diag from left to down
+    if set(flat_board[0:size**2:size+1]) == set(player):
+        return True
+    #check diag from right to down
+    if set(flat_board[size-1:size**2-1:size-1]) == set(player):
+        return True
 
+# check if the game is tie and print it
 def check_draw(BOARD):
     for i in BOARD:
         if '' in i:
@@ -76,21 +87,28 @@ def check_draw(BOARD):
     print('DRAW, no one win.')
     exit()
 
+# check if move was a win move and print it out
 def check_win(BOARD, row, column, player):
-    if check_row(BOARD, row, player) or check_coulumn(BOARD, column, player) or check_diag1(BOARD, player) or check_diag2(BOARD, player):
+    if check_row(BOARD, row, player) or check_coulumn(BOARD, column, player) or check_diag(BOARD, player):# or check_diag2(BOARD, player):
+        print_board(BOARD)
+        print(39 * '=')
         print('Congratulations, the player ' + player + ' WON!')
         exit()
 
+# change player
+def chenge_player(player):
+    return PLAYER_O if player == PLAYER_X else PLAYER_X
+
+#main
 def main():
 
     print_rules()
+    PLAYER = PLAYER_X
     while True:
         print_board(BOARD)
-        row, column = player_move(BOARD, PLAYER_X)
-        check_win(BOARD, row, column, PLAYER_X)
+        row, column = player_move(BOARD, PLAYER)
+        check_win(BOARD, row, column, PLAYER)
         check_draw(BOARD)
-        print_board(BOARD)
-        row, column = player_move(BOARD, PLAYER_O)
-        check_win(BOARD, row, column, PLAYER_O)
+        PLAYER = chenge_player(PLAYER)
 
 main()
